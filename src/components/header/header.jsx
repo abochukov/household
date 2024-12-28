d45a7c4078ba5dec814471dd25df1daae719bf38
Atanas Evgeniev Bochukov <abochukov@worldbankgroup.org>
2024-12-28 19:04:50 +0200

    Add dropdown to user icon in header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,48 @@
-// import React from 'react';
+import React, {useState, useEffect, iseRef, useRef} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear, faBell, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import './header.scss';
 
 const Header = () => {
+
+    const [userDropdownVisible, setUserDropdownVisible] = useState(false);
+    const userDropdownRef = useRef(null);
+    const userIconRef = useRef(null);
+
+    const handleUserClick = () => {
+        setUserDropdownVisible(!userDropdownVisible);
+    }
+
+    const handleUserClickOutside = (event) => {
+        if(userDropdownRef.current && !userDropdownRef.current.contains(event.target) && !userIconRef.current.contains(event.target)) {
+            setUserDropdownVisible(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleUserClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleUserClickOutside)
+        };
+    }, [])
+
     return(
         <div className="header-wrapper">
             <div className='toolbar-panel'>
                 <ul className="toolbar">
-                    <li><FontAwesomeIcon icon={faUser} /></li>
+                    <li>
+                        <FontAwesomeIcon icon={faUser} ref={userIconRef} onClick={handleUserClick} />
+                        {userDropdownVisible && (
+                            <div className="dropdown" ref={userDropdownRef}>
+                                <ul>
+                                    <li>Option 1</li>
+                                    <li>Излизане</li>
+                                </ul>
+                            </div>
+                        )}
+                    </li>
                     <li><FontAwesomeIcon icon={faBell} /></li>
                     <li><FontAwesomeIcon icon={faGear} /></li>
                 </ul>
