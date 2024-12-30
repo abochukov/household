95e71757aa8e37f905fe3107dcc2d68480c3164d
Atanas Evgeniev Bochukov <abochukov@worldbankgroup.org>
2024-12-30 17:52:02 +0200

    Add style for login component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+import './login.scss';
+
 function Login({ setIsAuthenticated }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -22,28 +24,36 @@ function Login({ setIsAuthenticated }) {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
+    <div className='login-wrapper'>
+      <div className='login-container'>
         <div>
-          <label>Username:</label>
-          <input 
-            type="text" 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-          />
+          <h2>Login</h2>
         </div>
-        <div>
-          <label>Password:</label>
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-          />
+        <div className='username'>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label>Потребителско име:</label>
+              <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+              />
+            </div>
+            <div className='password'>
+              <label>Парола:</label>
+              <input 
+                type="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+              />
+            </div>
+            {error && <p>{error}</p>}
+            <button type="submit">Login</button>
+          </form>
+
         </div>
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
-      </form>
+
+      </div>
     </div>
   );
 }
