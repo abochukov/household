c3cbd5d6fd06c5693f88d15fc7d8af309dcd3e6f
Atanas Evgeniev Bochukov <abochukov@worldbankgroup.org>
2025-02-22 19:56:55 +0200

    Add hamburger menu

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,58 +1,75 @@
-import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faWrench, faHouseFire, faMoneyBill, faUser, faRightFromBracket, faBarsProgress } from "@fortawesome/free-solid-svg-icons";
-
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faWrench, faHouseFire, faMoneyBill, faUser, faRightFromBracket, faBarsProgress, faBars } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 
 import './sidebar.scss';
 
 const SideBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Function to toggle the sidebar visibility
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Function to close the sidebar
+  const closeSidebar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="sidebar-container">
-      <ul>
-      <li>
-          <NavLink to="/home" activeClassName="active-link">
-            <FontAwesomeIcon icon={faHouse} />
-            <span>Начало</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/createAddress" activeClassName="active-link">
-            <FontAwesomeIcon icon={faWrench} />
-            <span>Нов адрес</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/createProperty" activeClassName="active-link">
-            <FontAwesomeIcon icon={faHouseFire} />
-            <span>Нов обект</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/manage" activeClassName="active-link">
-            <FontAwesomeIcon icon={faBarsProgress} />
-            <span>Управление</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/checkout" activeClassName="active-link">
-            <FontAwesomeIcon icon={faMoneyBill} />
-            <span>Каса</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/profile" activeClassName="active-link">
-            <FontAwesomeIcon icon={faUser} />
-            <span>Профил</span>
-          </NavLink>
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faRightFromBracket} />
-          <span>Изход</span>
-        </li>
-      </ul>
-    </div>
+    <>
+      {/* Hamburger Icon for Mobile */}
+      <div className="hamburger-menu" onClick={toggleSidebar}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      <div className={`sidebar-container ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li>
+            <NavLink to="/home" activeClassName="active-link" onClick={closeSidebar}>
+              <FontAwesomeIcon icon={faHouse} />
+              <span>Начало</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/createAddress" activeClassName="active-link" onClick={closeSidebar}>
+              <FontAwesomeIcon icon={faWrench} />
+              <span>Нов адрес</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/createProperty" activeClassName="active-link" onClick={closeSidebar}>
+              <FontAwesomeIcon icon={faHouseFire} />
+              <span>Нов обект</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/manage" activeClassName="active-link" onClick={closeSidebar}>
+              <FontAwesomeIcon icon={faBarsProgress} />
+              <span>Управление</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/checkout" activeClassName="active-link" onClick={closeSidebar}>
+              <FontAwesomeIcon icon={faMoneyBill} />
+              <span>Каса</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile" activeClassName="active-link" onClick={closeSidebar}>
+              <FontAwesomeIcon icon={faUser} />
+              <span>Профил</span>
+            </NavLink>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <span>Изход</span>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
