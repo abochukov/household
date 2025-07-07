cef83f8cabdc802de7f5b1e37e8310503b7231b4
Atanas Evgeniev Bochukov <abochukov@worldbankgroup.org>
2025-07-07 21:03:32 +0300

    Remove authenticate import

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-const authenticate = require('./authenticate');
-
 router.get('/', async (req, res) => {
     const username = req.query.username;
 
