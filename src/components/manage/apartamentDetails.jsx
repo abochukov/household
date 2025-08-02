d3dd56be5966bc3bfe37dd22e567f83cd2faa1a8
atanas.bochukov <atanas.bochukov@bulpros.com>
2025-08-02 09:51:00 +0300

    Disable edit address, city and neighbourhood

--- src/components/manage/apartamentDetails.jsx
+++ src/components/manage/apartamentDetails.jsx
@@ -344,49 +344,22 @@ const ApartamentDetails = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                <tr>
                         <td>Град</td>
                         <td>
-                            {isEditing ? (
-                                <input
-                                    type="text"
-                                    name="city"
-                                    value={formData.city}
-                                    onChange={handleInputChange}
-                                />
-                            ) : (
-                                apartament.city
-                            )}
+                            {apartament.city}
                         </td>
                     </tr>
                     <tr>
                         <td>Квартал</td>
                         <td>
-                            {isEditing ? (
-                                <input
-                                    type="text"
-                                    name="neighbourhood"
-                                    value={formData.neighbourhood}
-                                    onChange={handleInputChange}
-                                />
-                            ) : (
-                                apartament.neighbourhood
-                            )}
+                            {apartament.neighbourhood}
                         </td>
                     </tr>
                     <tr>
                         <td>Адрес</td>
                         <td>
-                            {isEditing ? (
-                                <input
-                                    type="text"
-                                    name="address"
-                                    value={formData.address}
-                                    onChange={handleInputChange}
-                                />
-                            ) : (
-                                apartament.address
-                            )}
+                           {apartament.address}
                         </td>
                     </tr>
                     <tr>
