3b5d33d6580bd5a759acd33f6d598160f6c9db5c
atanas.bochukov <atanas.bochukov@bulpros.com>
2025-08-07 18:10:28 +0300

    Fix db connection for cash module

--- server/routes/cash.js
+++ server/routes/cash.js
@@ -1,34 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg'); // Add pg import if missing
+const db = require('../db');
 
 const router = express.Router();
 
-const db = new Pool({
-  host: 'localhost',            // Database host (localhost for local machine)
-  port: 5432,                  // PostgreSQL default port
-  user: 'postgres',            // Your PostgreSQL username
-  password: 'postgres',            // Your PostgreSQL password
-  database: 'household',       // Your database name
-  max: 10,                     // Maximum number of connections in the pool
-});
-
-
-db.connect()
-  .then(client => {
-    return client.query('SELECT NOW()') // Perform a simple query to check the connection
-      .then(res => {
-        console.log('Connection successful:', res.rows[0]);
-        client.release(); // Release the client back to the pool
-      })
-      .catch(err => {
-        console.error('Error executing query:', err.stack);
-      });
-  })
-  .catch(err => {
-    console.error('Error connecting to the database:', err.stack);
-});
+const isProduction = process.env.NODE_ENV === 'production';
 
 router.get('/allResidentsForAddress', async (req, res) => {
     const username = req.query.username;
